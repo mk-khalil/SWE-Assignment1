06cc869bd29db03cee6cb61d28434be0f5e58649
mk-khalil <s-mohamed.khalil@zewailcity.edu.eg>
2023-05-12 03:26:44 +0300

    add unit tests for addition script

---
+++ test_addition.py
@@ -0,0 +1,19 @@
+import unittest
+from addition import add_numbers
+
+class TestAddition(unittest.TestCase):
+
+    def test_add_numbers_positive(self):
+        self.assertEqual(add_numbers(3, 5), 8, "Expected sum of 3 and 5 to be 8")
+
+    def test_add_numbers_negative(self):
+        self.assertEqual(add_numbers(-3, -5), -8, "Expected sum of -3 and -5 to be -8")
+
+    def test_add_numbers_mixed(self):
+        self.assertEqual(add_numbers(5, -3), 2, "Expected sum of 5 and -3 to be 2")
+
+    def test_add_numbers_zero(self):
+        self.assertEqual(add_numbers(0, 0), 0, "Expected sum of 0 and 0 to be 0")
+
+if __name__ == "__main__":
+    unittest.main()
