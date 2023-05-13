2ca89ff184cff6e2b9fc592a09cba6f78adc5959
mk-khalil <s-mohamed.khalil@zewailcity.edu.eg>
2023-05-13 19:14:21 +0300

    add test case for fractions

--- test_addition.py
+++ test_addition.py
@@ -14,6 +14,9 @@ class TestAddition(unittest.TestCase):
 
     def test_add_numbers_zero(self):
         self.assertEqual(add_numbers(0, 0), 0, "Expected sum of 0 and 0 to be 0")
+    
+    def test_add_fractions(self):
+        self.assertEqual(add_numbers(1/2, 1/2), 1, "Expected sum of 1/2 and 1/2 to be 1")
 
 if __name__ == "__main__":
     unittest.main()
